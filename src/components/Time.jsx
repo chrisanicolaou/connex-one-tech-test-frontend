215211a28a6108a96bb412638d33110f0e1b1598
Christos Nicolaou <chrisanicolaou>
2022-07-29 14:32:59 +0100

    Adds functionality for refreshes every 30 seconds

--- src/components/Time.jsx
+++ src/components/Time.jsx
@@ -1,20 +1,46 @@
 import React, { useState, useEffect } from "react";
 import getReq from "../utils/api";
-import ClientTime from "./ClientTime";
 
 function Time() {
   const [serverTime, setServerTime] = useState(0);
   const [isLoading, setIsLoading] = useState(true);
+  const [refetchTimer, setRefetchTimer] = useState(0);
 
   useEffect(() => {
+    refreshServerTime();
+    const interval = setInterval(() => {
+      setRefetchTimer((currentValue) => currentValue + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, [serverTime]);
+
+  useEffect(() => {
+    if (refetchTimer >= 30) {
+      setServerTime(0);
+    }
+  }, [refetchTimer]);
+
+  const refreshServerTime = async () => {
     setIsLoading(true);
-    const asyncUseEffect = async () => {
-      const result = await getReq("/time");
-      setIsLoading(false);
-      setServerTime(result.epoch);
-    };
-    asyncUseEffect();
-  }, [isLoading, serverTime]);
+    const result = await getReq("/time");
+    setIsLoading(false);
+    setRefetchTimer(0);
+    setServerTime(result.epoch);
+  };
+
+  const formatTimer = (num) => {
+    const hours = Math.floor(num / 3600);
+    const minutes = Math.floor(num / 3600 / 60);
+    const seconds = (num % 3600) % 60;
+    const timeArray = [hours, minutes, seconds].map((unitOfTime) => {
+      let returnString = unitOfTime.toString();
+      if (returnString.length < 2) {
+        returnString = `0${returnString}`;
+      }
+      return returnString;
+    });
+    return timeArray.join(":");
+  };
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -23,7 +49,7 @@ function Time() {
   return (
     <div>
       <p>Server Time: {serverTime}</p>
-      <ClientTime serverTime={serverTime} />
+      <p> Time since refresh: {formatTimer(refetchTimer)}</p>
     </div>
   );
 }
